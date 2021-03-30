1b4bfeb11f010911ae6b233dec8a064067dc0e41
Azim <66790056+Azim-js@users.noreply.github.com>
2021-03-30 13:44:52 +0530

    commit1

---
+++ assets/scripts/objects.js
@@ -0,0 +1,11 @@
+const person={
+    name:"Azim",
+    age:"21",
+    hobbies:["Football","cooking","coding"],
+    greet:function(){
+        alert("Hi my Name is Azim");
+    }
+};
+
+person.greet();  //is a method() in object
+console.log(person.name) //is a member variable with string value
