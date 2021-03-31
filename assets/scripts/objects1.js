7139efc522f1881671c9f43c5d3f275bc33a0a63
Azim <66790056+Azim-js@users.noreply.github.com>
2021-03-31 20:56:09 +0530

    //working with array and object to create a simple movie database

--- assets/scripts/objects1.js
+++ assets/scripts/objects1.js
@@ -1,1 +1,31 @@
 console.log("hi");
+
+const addMovieBtn=document.getElementById("add-movie-btn");
+const searchBtn=document.getElementById("search-btn");
+const movies=[];
+
+
+const addMovieHandler=()=>{
+    const title=document.getElementById("title").value;
+    const extraName=document.getElementById("extra-name").value;
+    const extraValue=document.getElementById("extra-value").value;
+
+    if(title.trim()==""|| extraName.trim()=="" || extraValue.trim()==""){
+        alert("please fill the details!!");
+        return;
+    }
+
+    const addMovies={
+        info:{
+            title,
+            [extraName]:extraValue
+        },
+        id:Math.random()
+    };
+   movies.push(addMovies);
+   console.log(addMovies);
+   console.log(movies.length);
+};
+
+
+addMovieBtn.addEventListener('click',addMovieHandler);
